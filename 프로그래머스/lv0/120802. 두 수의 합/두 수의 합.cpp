1d60e0e8389d204e2ee657c8bba97de2d9aa1eb0
honghanbeom <134994734+honghanbeom@users.noreply.github.com>
2023-06-05 21:20:26 +0900

    [level 0] Title: 두 수의 합, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    num1 >= -50000 && num1 >= 50000;
+    num2 >= -50000 && num2 >= 50000;
+    int answer = num1 + num2;
+    return answer;
+}
