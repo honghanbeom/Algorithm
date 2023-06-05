c04cc089803993b06154ba5670e94185fc1fc2a3
honghanbeom <134994734+honghanbeom@users.noreply.github.com>
2023-06-05 20:39:54 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    num1 > 0 && num1 <= 100;
+    num2 > 0 && num2 <= 100;
+    int answer = num1%num2;
+    return answer;
+}
