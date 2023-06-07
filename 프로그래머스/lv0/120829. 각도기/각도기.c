02b786ae9cfe0d757d791c815aaec88e55f86263
honghanbeom <134994734+honghanbeom@users.noreply.github.com>
2023-06-07 09:25:16 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int angle) {
+    int answer = 0;
+    angle > 0 && angle <= 180;
+    if(angle == 90)
+    {
+        answer = 2;
+        return answer;   
+    }
+    else if(angle > 0 && angle <= 90)
+    {
+        answer = 1;
+        return answer;
+    }
+    else if(angle >90 && angle < 180)
+    {
+        answer = 3;
+        return answer; 
+    }
+    else
+    {
+        answer = 4;
+        return answer;
+    }
+    return answer;
+}
