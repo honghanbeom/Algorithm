edadc878ff8b04d7e7ebbb619a524a77a6d3d313
honghanbeom <134994734+honghanbeom@users.noreply.github.com>
2024-04-01 09:04:49 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for(char c : my_string){
+        answer += tolower(c);
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}
