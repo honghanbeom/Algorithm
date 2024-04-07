273c0fc1839e89883c7d4aec62ebb2e5a32680f4
honghanbeom <134994734+honghanbeom@users.noreply.github.com>
2024-04-07 13:18:59 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    if(n % 6 == 0) return n/6;
+    else{
+        if(n % 2 == 0){
+            return (n/2);
+        }
+        else if(n % 3 == 0){
+            return (n/3);
+        }
+        else{
+            return n;
+        }
+    }
+}
